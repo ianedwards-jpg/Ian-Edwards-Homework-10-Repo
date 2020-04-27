368c066288cfcf88af87c4a376e42df3d43aa2e1
Ian Edwards <ian.edward@uconn.edu>
2020-04-27 09:28:07 -0400

    Update Employee.js

--- Repo/Develop/lib/Employee.js
+++ Repo/Develop/lib/Employee.js
@@ -1,65 +1,106 @@
 // TODO: Write code to define and export the Employee class
 const inquirer = require("inquirer");
-const axios= require("axios")
-const fs = require("fs")
+//const axios = require("axios")
+//const fs = require("fs")
 
-//Questions Array for README input
+//Questions Array for employee class
 const questions = [
-    {
-        name: 'title',
-        message: 'Give your project a name.',
-      },
-      {
-        name: 'description',
-        message: 'What is your project about? What does it do? What are you hoping to accomplish with it?',
-      },
-      {
-        name: 'installation',
-        message: 'Describe the installation process for your project. List any modules that will need to be installed for it to run.',
-      },
-      {
-        name: 'usage',
-        message: 'Provide instructions for the usage of your project. Include pictures as needed.',
-      },
-      {
-        name: 'license',
-        message: 'Describe what your project may and may not be used for. Include any special permissions that must be sought.',
-      },
-      {
-        name: 'contributing',
-        message: 'If you wish for others to build on your project, include instructions on accessing the project and guidelines for contribution.',
-      },
-      {
-        name: 'tests',
-        message: 'What kind of tests did you run?',
-      },
-    ]
+  {
+    name: 'name',
+    message: 'Enter your full name.',
+  },
+  {
+    name: 'id',
+    message: 'Enter your Employee ID.',
+  },
+  {
+    name: 'email',
+    message: 'Enter your email.'
+  }
+]
+function employeeData () {
+inquirer.prompt(questions).then(answers => {
+  console.info('Answer:', answers);
+  const data = json.stringify(answers)
+  module.exports = data;
+})
+  .catch(error => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
 
-    inquirer.prompt(questions).then(answers => {
-        console.info('Answer:', answers);
-        const data = json.stringify(answers)
-        module.exports = data;
-      })
-      .catch(error => {
-        if(error.isTtyError) {
-          // Prompt couldn't be rendered in the current environment
-        } else {
-          // Something else when wrong
-        }
-      });
-
-
-
-
-function writeToFile(fileName, data) {
-    
-    fs.writeFile("answerData.json", data, function(err) {
-
-        if (err) {
-          return console.log(err);
-        }
-      
-        console.log("Success!");
-      
-      });
+  chooseRole()
 }
+
+function chooseRole() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What is your role within our organization?",
+      choices: [
+        "Intern",
+        "Engineer",
+        "Manager",
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Intern":
+          artistSearch();
+          break;
+
+        case "Engineer":
+          multiSearch();
+          break;
+
+        case "Manager":
+          rangeSearch();
+          break;
+      }
+    });
+}
+
+employeeData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function writeToFile(fileName, data) {
+
+//   fs.writeFile("answerData.json", data, function (err) {
+
+//     if (err) {
+//       return console.log(err);
+//     }
+
+//     console.log("Success!");
+
+//   });
+// }
